a45cb6a54bb0d828fb5c3be15088c1865b749290
Alasdair-B <124393502+Alasdair-B@users.noreply.github.com>
2023-05-18 11:35:14 -0700

    commit on 'master'

--- Task1.py
+++ Task1.py
@@ -10,7 +10,7 @@ window.title('tk')
 style = Style()
 style.configure('TEnter', borderwidth = '3', relief=GROOVE)
 style.configure('TLabel', font=(BOLD,10))
-style.configure('TButton', borderwidth = '3', relief=GROOVE, font=BOLD, width=2)
+style.configure('TButton', borderwidth = 0, relief=GROOVE, font=(BOLD,12), width=1.5,ipady=0.5)
 style.configure('OutBox.TLabel', borderwidth = '3', width=10,background='#FFFFFF',font='#000000')
 
 Answer= StringVar(window,'')
@@ -33,11 +33,11 @@ def Calculate():
     Answer.set(x)
     
 
-enter1 = Entry(window,textvariable=Num1).grid(column=1,row=1,padx=5,pady=5)
-Label(window, text='X').grid(column=2,row=1,pady=5)
-enter2 = Entry(window,textvariable=Num2).grid(column=3,row=1,padx=5,pady=5)
-Button(window, text='=',command=Calculate).grid(column=4,row=1,pady=5)
-Out1 = Label(window,textvariable=Answer,style='OutBox.TLabel').grid(column=5,row=1,padx=5,pady=5)
+enter1 = Entry(window,textvariable=Num1).grid(column=1,row=1,padx=3,pady=1)
+Label(window, text='X').grid(column=2,row=1,pady=1)
+enter2 = Entry(window,textvariable=Num2).grid(column=3,row=1,padx=3,pady=1)
+Button(window, text='=',command=Calculate).grid(column=4,row=1,pady=1)
+Out1 = Label(window,textvariable=Answer,style='OutBox.TLabel').grid(column=5,row=1,padx=3,pady=1)
 
 
 window.mainloop()
